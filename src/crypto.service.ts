cfb8055e5b125d2aaab4355049b3e1c589736a90
Kamil Piękoś <kamil.piekos@10clouds.com>
2022-06-15 22:22:32 +0200

    move crypto key to env variables

--- src/crypto.service.ts
+++ src/crypto.service.ts
@@ -4,14 +4,17 @@ import * as crypto from 'crypto';
 @Injectable()
 export class CryptoService {
   public encrypt(text: string) {
-    const cipher = crypto.createCipher('aes-256-cbc', 'password');
+    const cipher = crypto.createCipher('aes-256-cbc', process.env.CRYPTO_KEY);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
   }
 
   public decrypt(text: string) {
-    const decipher = crypto.createDecipher('aes-256-cbc', 'password');
+    const decipher = crypto.createDecipher(
+      'aes-256-cbc',
+      process.env.CRYPTO_KEY,
+    );
     let decrypted = decipher.update(text, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
