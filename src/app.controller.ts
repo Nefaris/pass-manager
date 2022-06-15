b32ba91a0818381ed1f2f5fc19634f5bb465af27
Kamil Piękoś <kamil.piekos@10clouds.com>
2022-06-15 19:33:44 +0200

    implement adding passwords

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
   @Render('index')
-  index() {
-    return {
-      message: 'Hello from mvc controller!',
-    };
+  async index() {
+    const passwords = await this.prisma.password.findMany();
+    return { passwords };
+  }
+
+  @Post('/add-password')
+  async addPassword(
+    @Res() res: Response,
+    @Body() data: { name: string; username: string; password: string },
+  ) {
+    const created = await this.prisma.password.create({
+      data: {
+        name: data.name,
+        username: data.username,
+        password: data.password,
+      },
+    });
+
+    console.log(created);
+
+    res.redirect('/');
   }
 }
